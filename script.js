d63dd9e024c236a88d01c987a8a435ac123ccbe4
dtrabalho <daniellytrabalhoempresa@gmail.com>
2024-09-04 10:03:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const openDrawerBtn = document.getElementById('openDrawerBtn');
-    const closeDrawerBtn = document.getElementById('closeDrawerBtn');
-    const drawer = document.getElementById('drawer');
-    const panel = document.querySelector('.panel');
-    const message = document.createElement('div');
-
-    // Configura a mensagem
-    message.className = 'message';
-    message.innerHTML = `
-        <p>Entre na nossa comunidade e conheça diversos Billie Stans</p>
-        <button onclick="window.location.href='https://chat.whatsapp.com/CVFEPct6oAi2YSWc8wDQEE'">Junte-se a nós</button>
-    `;
-    document.body.appendChild(message);
-
-    openDrawerBtn.addEventListener('click', function () {
-        drawer.classList.add('open');
-    });
-
-    closeDrawerBtn.addEventListener('click', function () {
-        drawer.classList.remove('open');
-    });
-
-    panel.addEventListener('click', function () {
-        message.style.display = 'block';
-        setTimeout(() => {
-            message.style.display = 'none'; // Esconde a mensagem após 5 segundos
-        }, 5000); // 5 segundos
-    });
+// script.js
+document.getElementById('joinButton').addEventListener('click', function() {
+    alert('Bem-vindo à archive eilish\'s br! Estamos felizes em ter você conosco.');
+});
+// script.js
+document.getElementById('joinButton').addEventListener('click', function() {
+    window.location.href = 'https://dtrabalho.github.io/archiveeilishsbr-comunidade/';
 });

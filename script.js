d71813001492a8bcb634d0e6d2475745d4085f83
dtrabalho <daniellytrabalhoempresa@gmail.com>
2024-09-04 10:04:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// script.js
-document.getElementById('joinButton').addEventListener('click', function() {
-    alert('Bem-vindo à archive eilish\'s br! Estamos felizes em ter você conosco.');
-});
-// script.js
+
 document.getElementById('joinButton').addEventListener('click', function() {
     window.location.href = 'https://dtrabalho.github.io/archiveeilishsbr-comunidade/';
 });
